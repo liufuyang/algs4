01b6331611de50b6084c4ee4cc99b6cfe3d00a96
Fuyang Liu <liufuyang@users.noreply.github.com>
2021-02-28 09:12:43 +0100

    add pop

--- algs_stanford/examples/try_impl_linked_list_1/main.rs
+++ algs_stanford/examples/try_impl_linked_list_1/main.rs
@@ -21,11 +21,11 @@ mod link {
             &self.s
         }
 
-        // pub fn link(&mut self, n: Node) -> &mut Node {
-        //     // or use option.insert()
-        //     self.next = Some(Box::new(n));
-        //     self.next.as_deref_mut().unwrap()
-        // }
+        pub fn link(&mut self, n: Node) -> &mut Node {
+            // or use option.insert()
+            self.next = Some(Box::new(n));
+            self.next.as_deref_mut().unwrap()
+        }
     }
 
     pub struct LinkedList {
@@ -44,7 +44,7 @@ mod link {
             while let Some(ref mut next) = tail.next {
                 tail = next;
             }
-            tail.next = Some(Box::new(Node::new(s)));
+            tail.link(Node::new(s));
             self
         }
 
@@ -60,30 +60,39 @@ mod link {
         // }
 
         /// pop element from tail
-        // pub fn pop_tail(&mut self) -> Option<String> {
-        //     if self.head.next.is_none() {
-        //         return None;
-        //     }
-        //
-        //     let mut one_before_tail = &mut self.head;
-        //     while let Some(ref mut next_before) = one_before_tail.next {
-        //         if let Some(ref mut next) = next_before.next {
-        //             one_before_tail = next;
-        //         } else {
-        //             break;
-        //         }
-        //     }
-        //
-        //     let last = one_before_tail.next.take();
-        //     last.map(|n| n.s)
-        // }
+        pub fn pop_tail(&mut self) -> Option<String> {
+            if self.head.next.is_none() {
+                return None;
+            }
+
+            let mut one_before_tail = &mut self.head;
+            while let Some(ref mut next_before) = one_before_tail.next {
+                if let Some(ref mut _next) = next_before.next {
+                    one_before_tail = next_before;
+                } else {
+                    break;
+                }
+            }
+
+            let last = one_before_tail.next.take();
+            last.map(|n| n.s)
+        }
 
         /// push element from head
         pub fn push(&mut self, s: String) -> &mut LinkedList {
             let mut new_head = Node::new(s);
             new_head.next = self.head.next.take();
-            self.head.next = Some(Box::new(new_head));
+            self.head.link(new_head);
             self
+        }
+
+        /// pop element from head
+        pub fn pop(&mut self) -> Option<String> {
+            if let Some(first) = self.head.next.take() {
+                self.head.next = first.next;
+                return Some(first.s);
+            }
+            None
         }
 
         pub fn iter(&self) -> Iter {
@@ -125,11 +134,21 @@ fn main() {
     }
     println!();
 
-    // println!("pop: {}", list.pop_tail().unwrap_or("?".to_string()));
-    // for s in list.iter() {
-    //     print!("{},", s)
-    // }
-    // println!();
+    println!("pop head: {}", list.pop().unwrap_or("?".to_string()));
+    println!("pop head: {}", list.pop().unwrap_or("?".to_string()));
+
+    println!("pop tail: {}", list.pop_tail().unwrap_or("?".to_string()));
+    println!("pop tail: {}", list.pop_tail().unwrap_or("?".to_string()));
+
+    for s in list.iter() {
+        print!("{},", s)
+    }
+    println!();
+
+    println!("pop head: {}", list.pop().unwrap_or("?".to_string()));
+    println!("pop tail: {}", list.pop_tail().unwrap_or("?".to_string()));
+    println!("pop head: {}", list.pop().unwrap_or("?".to_string()));
+    println!("pop tail: {}", list.pop_tail().unwrap_or("?".to_string()));
 }
 
 // If normal borrow checker cannot work, then the borrow checker bug still exist, use
