794f33309d0bdecd5939f3695238a8a85652795c
Fuyang Liu <liufuyang@users.noreply.github.com>
2021-05-15 08:51:00 +0200

    princeton week4 mostly working

--- algs_princeton/4_a_start_search_pq/Board.java
+++ algs_princeton/4_a_start_search_pq/Board.java
@@ -1,3 +1,5 @@
+import edu.princeton.cs.algs4.Queue;
+
 public class Board {
 
   private final int[][] tiles;
@@ -7,12 +9,20 @@ public class Board {
   // create a board from an n-by-n array of tiles,
   // where tiles[row][col] = tile at (row, col)
   public Board(int[][] tiles) {
-    this.tiles = tiles;
+    if (tiles == null) {
+      throw new IllegalArgumentException();
+    }
+    int d = tiles.length;
+    int[][] newTiles = new int[d][d];
+    for (int r = 0; r < d; r++) {
+      System.arraycopy(tiles[r], 0, newTiles[r], 0, d);
+    }
+    this.tiles = newTiles;
   }
 
   // string representation of this board
   public String toString() {
-    StringBuffer buffer = new StringBuffer(this.dimension() + "\n");
+    StringBuilder buffer = new StringBuilder(this.dimension() + "\n");
     int maxDigit = (int) Math.log10(this.dimension() * this.dimension());
     for (int[] row : tiles) {
       for (int n : row) {
@@ -37,8 +47,7 @@ public class Board {
     for (int r = 0; r < dimension(); r++) {
       for (int c = 0; c < dimension(); c++) {
         int correct = dimension() * (r) + c + 1;
-        if (correct == dimension() * dimension()) break;
-        if (tiles[r][c] != correct) tmp++;
+        if (tiles[r][c] != correct && !(r == dimension() - 1 && c == dimension() - 1)) tmp++;
       }
     }
     hamming = tmp;
@@ -56,7 +65,9 @@ public class Board {
         if (tiles[r][c] == 0) {
           continue;
         } else {
-          tmp += Math.abs((tiles[r][c] - 1) % dimension() - c) + Math.abs((tiles[r][c] - 1) / dimension() - r);
+          tmp +=
+              Math.abs((tiles[r][c] - 1) % dimension() - c)
+                  + Math.abs((tiles[r][c] - 1) / dimension() - r);
         }
       }
     }
@@ -65,16 +76,109 @@ public class Board {
   }
 
   //  // is this board the goal board?
-  //  public boolean isGoal()
-  //
+  public boolean isGoal() {
+    return this.hamming() == 0;
+  }
+
   //  // does this board equal y?
-  //  public boolean equals(Object y)
+  @Override
+  public boolean equals(Object that) {
+    if (this == that) {
+      return true;
+    }
+    if (that == null || getClass() != that.getClass()) {
+      return false;
+    }
+    Board board = (Board) that;
+
+    if (this.dimension() != board.dimension()) return false;
+    for (int r = 0; r < dimension(); r++) {
+      for (int c = 0; c < dimension(); c++) {
+        if (this.tiles[r][c] != board.tiles[r][c]) return false;
+      }
+    }
+    return true;
+  }
+
   //
   //  // all neighboring boards
-  //  public Iterable<Board> neighbors()
-  //
+  public Iterable<Board> neighbors() {
+    int zeroR = -1;
+    int zeroC = -1;
+    for (int r = 0; r < dimension(); r++) {
+      for (int c = 0; c < dimension(); c++) {
+        if (tiles[r][c] == 0) {
+          zeroR = r;
+          zeroC = c;
+        }
+      }
+    }
+
+    Queue<Board> queue = new Queue<>();
+    // Go down
+    if (zeroR < dimension() - 1) {
+      Board newBoard = this.cloneBoard();
+      newBoard.swap(zeroR, zeroC, zeroR + 1, zeroC);
+      queue.enqueue(newBoard);
+    }
+    // Go right
+    if (zeroC < dimension() - 1) {
+      Board newBoard = this.cloneBoard();
+      newBoard.swap(zeroR, zeroC, zeroR, zeroC + 1);
+      queue.enqueue(newBoard);
+    }
+    // Go left
+    if (zeroC > 0) {
+      Board newBoard = this.cloneBoard();
+      newBoard.swap(zeroR, zeroC, zeroR, zeroC - 1);
+      queue.enqueue(newBoard);
+    }
+    // Go up
+    if (zeroR > 0) {
+      Board newBoard = this.cloneBoard();
+      newBoard.swap(zeroR, zeroC, zeroR - 1, zeroC);
+      queue.enqueue(newBoard);
+    }
+    return queue;
+  }
+
+  private void swap(int aR, int aC, int bR, int bC) {
+    int n = this.tiles[aR][aC];
+    this.tiles[aR][aC] = this.tiles[bR][bC];
+    this.tiles[bR][bC] = n;
+  }
+
   //  // a board that is obtained by exchanging any pair of tiles
-  //  public Board twin()
+  public Board twin() {
+    Board clone = this.cloneBoard();
+    int aR = -1;
+    int aC = -1;
+    int bR = -1;
+    int bC = -1;
+    for (int r = 0; r < dimension(); r++) {
+      for (int c = 0; c < dimension(); c++) {
+        if ((aR == -1 || aC == -1) && clone.tiles[r][c] != 0) {
+          aR = r;
+          aC = c;
+        } else if ((bR == -1 || bC == -1) && clone.tiles[r][c] != 0) {
+          bR = r;
+          bC = c;
+        } else if (aR != -1 && aC != -1 && bR != -1 && bC != -1) {
+          break;
+        }
+      }
+    }
+    clone.swap(aR, aC, bR, bC);
+    return clone;
+  }
+
+  private Board cloneBoard() {
+    int[][] newTiles = new int[dimension()][dimension()];
+    for (int r = 0; r < dimension(); r++) {
+      if (dimension() >= 0) System.arraycopy(this.tiles[r], 0, newTiles[r], 0, dimension());
+    }
+    return new Board(newTiles);
+  }
 
   // unit testing (not graded)
   public static void main(String[] args) {
--- algs_princeton/4_a_start_search_pq/Solver.java
+++ algs_princeton/4_a_start_search_pq/Solver.java
@@ -1,31 +1,138 @@
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Stack;
+
 public class Solver {
-  final Board initial;
+
+  private final Stack<Board> boardStack = new Stack<>();
+  private final int move;
 
   // find a solution to the initial board (using the A* algorithm)
   public Solver(Board initial) {
-    this.initial = initial;
-    // TODO
-    System.out.println(initial);
-    System.out.println("hamming: " + initial.hamming());
-    System.out.println("manhattan: " + initial.manhattan());
+    if (initial == null) {
+      throw new IllegalArgumentException();
+    }
+    MinPQ<State> frontierInitial = new MinPQ<>();
+    MinPQ<State> frontierTwin = new MinPQ<>();
+    Board twin = initial.twin();
+    frontierInitial.insert(new State(initial));
+    frontierTwin.insert(new State(twin));
+
+    State currentInitial;
+    State currentTwin;
+    while (true) {
+      currentInitial = solve1Step(frontierInitial);
+      currentTwin = solve1Step(frontierTwin);
+      if (currentTwin.getBoard().isGoal()) {
+        move = -1;
+        return;
+      }
+      if (currentInitial.getBoard().isGoal()) {
+        move = currentInitial.move;
+        State end = currentInitial;
+        while (end != null) {
+          boardStack.push(end.board);
+          end = end.prev;
+        }
+        return;
+      }
+    }
+  }
+
+  private State solve1Step(MinPQ<State> frontier) {
+    State state = frontier.delMin();
+    state
+        .getBoard()
+        .neighbors()
+        .forEach(
+            neighbor -> {
+              State prev = state;
+              boolean visited = false;
+              while (prev != null) {
+                if (prev.board.equals(neighbor)) {
+                  visited = true;
+                  break;
+                }
+                prev = prev.prev;
+              }
+              if (!visited) {
+                frontier.insert(state.chain(neighbor));
+              }
+            });
+    //    System.out.println("frontier size:" + frontier.size());
+    //    System.out.println("visited size:" + visited.size());
+    //    System.out.println(state.board);
+    return state;
   }
 
   // is the initial board solvable? (see below)
   public boolean isSolvable() {
-    return false;
+    return move > -1;
   }
 
   // min number of moves to solve initial board; -1 if unsolvable
   public int moves() {
-
-    return 0;
+    return move;
   }
 
   // sequence of boards in a shortest solution; null if unsolvable
   public Iterable<Board> solution() {
-    return null;
+    if (!isSolvable()) return null;
+    return boardStack;
+  }
+
+  private class State implements Comparable<State> {
+    private final Board board;
+    private final int move;
+    private final State prev;
+
+    public State(Board board) {
+      this.board = board;
+      this.move = 0;
+      this.prev = null;
+    }
+
+    public State(Board board, int move, State prev) {
+      this.board = board;
+      this.move = move;
+      this.prev = prev;
+    }
+
+    /** IMPORTANT: increase move number by one when chaining */
+    public State chain(Board newBoard) {
+      return new State(newBoard, this.move + 1, this);
+    }
+
+    public Board getBoard() {
+      return board;
+    }
+
+    private int weight() {
+      return board.manhattan() + move;
+    }
+
+    @Override
+    public int compareTo(State that) {
+      return Integer.compare(this.weight(), that.weight());
+    }
   }
 
   // test client (see below)
-  public static void main(String[] args) {}
+//  public static void main(String[] args) {
+//    // create initial board from file
+//    In in = new In(args[0]);
+//    int n = in.readInt();
+//    int[][] tiles = new int[n][n];
+//    for (int i = 0; i < n; i++) for (int j = 0; j < n; j++) tiles[i][j] = in.readInt();
+//    Board initial = new Board(tiles);
+//
+//    // solve the puzzle
+//    Solver solver = new Solver(initial);
+//
+//    // print solution to standard output
+//    if (!solver.isSolvable()) StdOut.println("No solution possible");
+//    else {
+//      StdOut.println("Minimum number of moves = " + solver.moves());
+//      for (Board board : solver.solution()) StdOut.println(board);
+//    }
+//  }
 }
